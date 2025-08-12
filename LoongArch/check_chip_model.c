6fe8db8bb6a130bbc8c9958cf4d89edb5e1e09ed
Dongyan Qian <qiandongyan@loongson.cn>
2025-08-12 17:44:18 +0800

    check_chip_model: get chip vid/name
    usages: sudo ./OsTools chip -r -m 1

--- LoongArch/check_chip_model.c
+++ LoongArch/check_chip_model.c
@@ -1,15 +1,51 @@
 // SPDX-License-Identifier: GPL-2.0
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include <fcntl.h>
 #include "def.h"
 
 #define REG_3C6000_BA_DIFF 0x1fe00470ULL
+#define REG_CHIP_VID_OFFSET  0x10
+#define REG_CHIP_NAME_OFFSET 0x20
 
 static const char *const check_model_usages[] = {
     PROGRAM_NAME" check_model <args>",
     NULL,
 };
+
+int read_model1_ascii (void)
+{
+    void * p = NULL;
+    int status ;
+    unsigned long long devaddr;
+
+    int fd = open ("/dev/mem", O_RDWR | O_SYNC);
+    if (fd < 0) {
+        printf("can't open file,please use root .\n");
+        return 1;
+    }
+
+    devaddr = 0x1fe00000ULL;
+
+    /*Transfer Virtul to Phy Addr*/
+    p = vtpa (devaddr, fd);
+
+    printf("Read Chip VID/NAME Start ...\n");
+    unsigned char buf_vid[16];
+    unsigned char buf_name[16];
+
+    memset(buf_vid, 0, 0x10);
+    memcpy(buf_vid, p + REG_CHIP_VID_OFFSET, 0x10);
+    memset(buf_name, 0, 0x10);
+    memcpy(buf_name, p + REG_CHIP_NAME_OFFSET, 0x10);
+
+    printf ("This is \033[32m VID/CHIP: %s/%s \033[0m. \n", buf_vid, buf_name);
+
+    status = releaseMem(p);
+    close(fd);
+    return status;
+}
 
 int check_model_read (void)
 {
@@ -48,12 +84,14 @@ int cmd_chip_model (int argc, const char **argv)
 {
     int read = 0;
     int write = 0;
+    int model = 0;
     uid_t uid;
     struct argparse argparse;
 
     struct argparse_option options[] = {
         OPT_HELP(),
         OPT_BOOLEAN ('r', "read", &read, "read model", NULL, 0, 0),
+        OPT_INTEGER ('m', "model", &model, "model !0", NULL, 0, 0),
         OPT_END(),
     };
 
@@ -72,7 +110,11 @@ int cmd_chip_model (int argc, const char **argv)
     }
 
     if (read) {
-        check_model_read ();
+        if (model == 0) {
+            check_model_read ();
+	} else if (model == 1) { //read ascii
+            read_model1_ascii ();
+        }
     }
     return 0;
 }
